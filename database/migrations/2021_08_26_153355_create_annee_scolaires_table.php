867326ab079aa5c087465c7fd13f5d173731ac2d
Ashley-23 <ashleysatchivi92@gmail.com>
2021-08-26 15:35:06 +0000

    Création du model AnneeScolaire et de sa migration

---
+++ database/migrations/2021_08_26_153355_create_annee_scolaires_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateAnneeScolairesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('annee_scolaires', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('annee_scolaires');
+    }
+}
