538ff0b383ec5e3a565bcbac9e8f1af30be22015
Ashley-23 <ashleysatchivi92@gmail.com>
2021-08-26 15:25:33 +0000

    Ajout des attributs de Professeur

--- database/migrations/2021_08_26_152104_create_professeurs_table.php
+++ database/migrations/2021_08_26_152104_create_professeurs_table.php
@@ -14,8 +14,22 @@ class CreateProfesseursTable extends Migration
     public function up()
     {
         Schema::create('professeurs', function (Blueprint $table) {
-            $table->id();
+            $table->id('idProfesseur');
+            $table->string('nomProfesseur');
+            $table->string('prenomProfesseur');
+            $table->date('dateNaissProfesseur');
+            $table->char('sexeProfesseur', 1);
+            $table->string('emailProfesseur')->unique();
+            $table->string('passwordProfesseur');
+            $table->string('telephoneProfesseur');
+            $table->string('PhotoProfesseur')->default('photoProfesseur.jpg');
+            $table->String('adresseProfesseur');
+            $table->char('activeProfesseur', 1);
             $table->timestamps();
+
+
+
+            $table->foreignId('idProfil')->constrained();
         });
     }
 
