3ce4feea0098cff461b5e01bf2627e925d48429b
Ashley-23 <ashleysatchivi92@gmail.com>
2021-08-26 17:06:43 +0000

    Ajout des elements de Enseignement, des foreignkeys et importation de leurs classes

--- database/migrations/2021_08_26_170056_create_enseignements_table.php
+++ database/migrations/2021_08_26_170056_create_enseignements_table.php
@@ -1,5 +1,8 @@
 <?php
 
+use App\Models\Classe;
+use App\Models\EpreuveMatiereNote;
+use App\Models\Professeur;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -14,8 +17,15 @@ class CreateEnseignementsTable extends Migration
     public function up()
     {
         Schema::create('enseignements', function (Blueprint $table) {
-            $table->id();
+            $table->id('idEnseignement');
+            $table->string('descEnseignement');
+            $table->char('activeEnseignement');
             $table->timestamps();
+
+
+            $table->foreignId('idClasse')->constrained();
+            $table->foreignId('idEpreuveMatiereNote')->constrained();
+            $table->foreignId('idProfesseur')->constrained();
         });
     }
 
