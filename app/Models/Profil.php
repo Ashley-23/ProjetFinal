dbf8c48dc8c459b1c40d375b7314c524d53ce4a0
Ashley-23 <ashleysatchivi92@gmail.com>
2021-08-26 14:33:28 +0000

    Ajout de la relation entre parent et profil

--- app/Models/Profil.php
+++ app/Models/Profil.php
@@ -3,6 +3,7 @@
 namespace App\Models;
 
 use App\Models\Admin;
+use App\Models\Parent;
 use App\Models\SuperAdmin;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
