c91a13f5a02b7f7d8260f89f5671bba6bb7d6930
Ashley-23 <ashleysatchivi92@gmail.com>
2021-08-27 12:21:18 +0000

    Ajout des relations entre le model relation eleve  et les autres models

--- app/Models/RelationEleve.php
+++ app/Models/RelationEleve.php
@@ -2,10 +2,25 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
+use App\Models\Eleve;
+use App\Models\Relative;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class RelationEleve extends Model
 {
     use HasFactory;
+
+    // // Une relation eleve appartient à un eleve
+    public function Eleve()
+    {
+        return $this->belongsTo(Eleve::class);
+    }
+
+
+    // // Une relation eleve appartient à un relative
+    public function Relative()
+    {
+        return $this->belongsTo(Relative::class);
+    }
 }
