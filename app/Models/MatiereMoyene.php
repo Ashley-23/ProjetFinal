0d2bc29188c97a558ae60e7552fabcdb6a335429
Ashley-23 <ashleysatchivi92@gmail.com>
2021-08-27 10:44:14 +0000

    Ajout des relations entre matiereMoyenne et les autres models

--- app/Models/MatiereMoyene.php
+++ app/Models/MatiereMoyene.php
@@ -2,10 +2,33 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
+use App\Models\Matiere;
+use App\Models\Moyenne;
+use App\Models\Bulletin;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class MatiereMoyene extends Model
 {
     use HasFactory;
+
+    // // Une MatiereMoyenne appartient à une matiere
+    public function Matiere()
+    {
+        return $this->belongsTo(Matiere::class);
+    }
+
+    // // Une MatiereMoyenne appartient à une moyenne
+    public function Moyenne()
+    {
+        return $this->belongsTo(Moyenne::class);
+    }
+
+
+    // Une MatiereMoyenne a un plusieurs bulletins
+
+    public function Bulletins()
+    {
+        return $this->hasMany(Bulletin::class);
+    }
 }
