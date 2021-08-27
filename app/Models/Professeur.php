2415d3ec7d5b632ece9f89bff962e667ac4c8da9
Ashley-23 <ashleysatchivi92@gmail.com>
2021-08-27 09:57:15 +0000

    Ajout des relations entre le model Professeur et les autres models

--- app/Models/Professeur.php
+++ app/Models/Professeur.php
@@ -3,6 +3,7 @@
 namespace App\Models;
 
 use App\Models\Profil;
+use App\Models\Enseignement;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 
@@ -13,5 +14,11 @@ class Professeur extends Model
     public function Profil()
     {
         return $this->hasOne(Profil::class);
+    }
+    // Un professeur a un plusieurs Enseignement
+
+    public function Enseignements()
+    {
+        return $this->hasMany(Enseignement::class);
     }
 }
