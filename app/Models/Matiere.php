65ccc005a41843744f2b42e039265daf668caefa
Ashley-23 <ashleysatchivi92@gmail.com>
2021-08-27 10:39:17 +0000

    Ajout des relations entre le model Matiere et les autres models

--- app/Models/Matiere.php
+++ app/Models/Matiere.php
@@ -2,10 +2,26 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
+use App\Models\MatiereMoyene;
+use App\Models\EpreuveMatiereNote;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Matiere extends Model
 {
     use HasFactory;
+
+    // Une matiere a  plusieurs MatiereMoyenne
+
+    public function MatiereMoyennes()
+    {
+        return $this->hasMany(MatiereMoyene::class);
+    }
+
+    // Une matiere a  plusieurs EpreuveMatiereNote 
+
+    public function EpreuveMatiereNotes()
+    {
+        return $this->hasMany(EpreuveMatiereNote::class);
+    }
 }
