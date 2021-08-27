1547cc5c2712f8344a738aec514ba289d023de26
Ashley-23 <ashleysatchivi92@gmail.com>
2021-08-27 09:54:10 +0000

    Ajout des relations entre le Modele Moyenne et les autres models

--- app/Models/Moyenne.php
+++ app/Models/Moyenne.php
@@ -2,10 +2,17 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
+use App\Models\MatiereMoyene;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Moyenne extends Model
 {
     use HasFactory;
+    // Une moyenne a un plusieurs MatiereMoyenne
+
+    public function MatiereMoyennes()
+    {
+        return $this->hasMany(MatiereMoyene::class);
+    }
 }
