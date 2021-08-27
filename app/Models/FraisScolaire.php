6835b6442d1129796140a8f1016c8583bd19c32c
Ashley-23 <ashleysatchivi92@gmail.com>
2021-08-27 10:22:37 +0000

    Modification des relations entre frais scolaire et type frais

--- app/Models/FraisScolaire.php
+++ app/Models/FraisScolaire.php
@@ -2,10 +2,18 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
+use App\Models\TypeFrais;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class FraisScolaire extends Model
 {
     use HasFactory;
+
+
+    // // Un frais scolaire appartient à un type frais
+    public function TypeFrais()
+    {
+        return $this->belongsTo(TypeFrais::class);
+    }
 }
