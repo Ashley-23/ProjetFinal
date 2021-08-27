425c758891b8ac3c6468bba380449b05872e82d5
Ashley-23 <ashleysatchivi92@gmail.com>
2021-08-27 11:28:39 +0000

    Ajout des attributs de la table resultant du hasMany des tables Etablissement et Realtive qui désigne parent

--- database/migrations/2021_08_27_112120_create_etablissement_relatives_table.php
+++ database/migrations/2021_08_27_112120_create_etablissement_relatives_table.php
@@ -13,8 +13,10 @@ class CreateEtablissementRelativesTable extends Migration
      */
     public function up()
     {
-        Schema::create('etablissement_relatives', function (Blueprint $table) {
+        Schema::create('etablissement_relative', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('idEtablisssement')->constrained()->onDelete('cascade');
+            $table->foreignId('idRelative')->constrained()->onDelete('cascade');
             $table->timestamps();
         });
     }
