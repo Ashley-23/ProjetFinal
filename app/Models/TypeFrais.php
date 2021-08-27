edd5b3d8accbcee9dc9f174a7123d21ddae226c1
Ashley-23 <ashleysatchivi92@gmail.com>
2021-08-27 10:26:07 +0000

    Ajout des relations entre le model TypeFrais et les autres models

--- app/Models/TypeFrais.php
+++ app/Models/TypeFrais.php
@@ -3,10 +3,18 @@
 namespace App\Models;
 
 
+use App\Models\FraisScolaire;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class TypeFrais extends Model
 {
     use HasFactory;
+
+    // Un type frais a un plusieurs frais scolaire
+
+    public function FraisScolaires()
+    {
+        return $this->hasMany(FraisScolaire::class);
+    }
 }
