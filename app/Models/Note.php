e27991fa276a3e211ffc026d537f5fa4c343a888
Ashley-23 <ashleysatchivi92@gmail.com>
2021-08-27 09:51:49 +0000

    Ajout des relations entre le models Note et les autres Models

--- app/Models/Note.php
+++ app/Models/Note.php
@@ -2,10 +2,17 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
+use App\Models\EpreuveMatiereNote;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Note extends Model
 {
     use HasFactory;
+    // Une Note a un plusieurs EpreuveMatiereNote
+
+    public function EpreuveMatiereNotes()
+    {
+        return $this->hasMany(EpreuveMatiereNote::class);
+    }
 }
