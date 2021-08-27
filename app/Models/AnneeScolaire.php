768a61ce4c27ae03bf466060d6b0f8c6887c65dc
Ashley-23 <ashleysatchivi92@gmail.com>
2021-08-27 09:59:51 +0000

    Ajout des relations entre le model Inscription et les autres models

--- app/Models/AnneeScolaire.php
+++ app/Models/AnneeScolaire.php
@@ -2,10 +2,17 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
+use App\Models\Inscription;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class AnneeScolaire extends Model
 {
     use HasFactory;
+    // Une annee scolaire a un plusieurs Inscriptions
+
+    public function Inscriptions()
+    {
+        return $this->hasMany(Inscription::class);
+    }
 }
