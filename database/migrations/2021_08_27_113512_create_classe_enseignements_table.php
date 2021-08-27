f4a81c05e1f3579f0e75a962e7445bcb262ab2e4
Ashley-23 <ashleysatchivi92@gmail.com>
2021-08-27 11:38:33 +0000

    Ajout des elements de la migration ClasseEnseignement

--- database/migrations/2021_08_27_113512_create_classe_enseignements_table.php
+++ database/migrations/2021_08_27_113512_create_classe_enseignements_table.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Models\Classe;
+use App\Models\Enseignement;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -13,8 +15,11 @@ class CreateClasseEnseignementsTable extends Migration
      */
     public function up()
     {
-        Schema::create('classe_enseignements', function (Blueprint $table) {
+        Schema::create('classe_enseignement', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('idClasse')->constrained()->onDelete('cascade');
+            $table->foreignId('idEnseignement')->constrained()->onDelete('cascade');
+
             $table->timestamps();
         });
     }
