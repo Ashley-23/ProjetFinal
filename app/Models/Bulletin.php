6dbe481fb25ad133bae8be96b2e5f8deb00aae05
Ashley-23 <ashleysatchivi92@gmail.com>
2021-08-27 12:06:34 +0000

    Ajout des relations entre le model bulletin et les autres models

--- app/Models/Bulletin.php
+++ app/Models/Bulletin.php
@@ -2,10 +2,26 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
+use App\Models\Eleve;
+use App\Models\MatiereMoyene;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Bulletin extends Model
 {
     use HasFactory;
+
+
+    // // Un bulletin appartient à un eleve
+    public function Eleve()
+    {
+        return $this->belongsTo(Eleve::class);
+    }
+
+
+    // // Un bulletin appartient à un matiere moyenne
+    public function MatiereMoyene()
+    {
+        return $this->belongsTo(MatiereMoyene::class);
+    }
 }
