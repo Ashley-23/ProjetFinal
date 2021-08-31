2c2ee2d508a2d4940e5fae943f1115bbb9f36cf1
Ashley-23 <ashleysatchivi92@gmail.com>
2021-08-31 11:21:39 +0000

    Appelle des routes admin crées via le controller adminAffController dans le fichier web .php

--- app/Http/Controllers/AdminAffController.php
+++ app/Http/Controllers/AdminAffController.php
@@ -15,14 +15,184 @@ class AdminAffController extends Controller
         // 
         return view('admin.accueil');
     }
-
+    //                                                      ELEVES
 
     // Pour afficher la page de la liste de tous les élèves 
-    public function allstudents()
+    public function allstudent()
     {
         // 
-        return view('admin.Students.allstudent')
+        return view('admin.Student.allstudent');
     }
+    // Detail sur les eleves 
+    public function detailstudent()
+    {
+        // 
+        return view('admin.Student.detailstudent');
+    }
+    // Ajouter un eleve 
+    public function addstudent()
+    {
+        // 
+        return view('admin.Student.add');
+    }
+
+
+
+
+    //                                                         Professeurs
+    // Pour afficher la liste de tous les professeurs 
+    public function allteacher()
+    {
+        // 
+        return view('admin.Teacher.allteacher');
+    }
+    // Pour afficher les details sur un professeur 
+    public function detailteacher()
+    {
+        // 
+        return view('admin.Teacher.detailteacher');
+    }
+    //   ajouter un professeur
+    public function addteacher()
+    {
+        // 
+        return view('admin.Teacher.addteacher');
+    }
+
+    //                                                                 Parents
+    // Afficher la liste des parents 
+    public function allparent()
+    {
+        // 
+        return view('admin.Parent.allparent');
+    }
+    // Details sur les parents 
+    public function detailparent()
+    {
+        // 
+        return view('admin.Parent.detailparent');
+    }
+    // ajouter un parent 
+    public function addparent()
+    {
+        // 
+        return view('admin.Parent.addparent');
+    }
+
+
+
+    //                                                                    Comptes
+
+    // Pour afficher les frais scolaires 
+    public function fraisscolaire()
+    {
+        // 
+        return view('admin.Compte.fraisscolaire');
+    }
+    // Pour afficher les dépenses 
+    public function depense()
+    {
+        // 
+        return view('admin.Compte.depense');
+    }
+    // Pour ajouter des frais 
+    public function addfrais()
+    {
+        // 
+        return view('admin.Compte.addfrais');
+    }
+
+
+
+    //                                                               CLASSES
+    // Toutes les classes 
+    public function allclass()
+    {
+        // 
+        return view('admin.classe.allclass');
+    }
+    // Ajouter une classe 
+    public function addclass()
+    {
+        // 
+        return view('admin.classe.addclass');
+    }
+
+
+    //                                                                  Matiere
+    public function subject()
+    {
+        // 
+        return view('admin.Subject');
+    }
+    //                                                                  CLASSE ROUTINE
+    public function classroutine()
+    {
+        // 
+        return view('admin.classroutine');
+    }
+    //                                                                  PRESENCE
+    public function presence()
+    {
+        // 
+        return view('admin.presence');
+    }
+    //                                                                  NOTIFICATION
+    public function notice()
+    {
+        // 
+        return view('admin.notice');
+    }
+    //                                                                  MESSAGES
+    public function Message()
+    {
+        // 
+        return view('admin.message');
+    }
+    //                                                                  COMPTE
+    public function account()
+    {
+        // 
+
+        return view('admin.account');
+    }
+
+    //                                                                  EXAMS 
+
+    // pour les horaires 
+    public function horaire()
+    {
+        // 
+
+        return view('admin.Examen.horaire');
+    }
+    // pour les notes 
+    public function mark()
+    {
+        // 
+
+        return view('admin.Examen.mark');
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
     /**
      * Display a listing of the resource.
--- routes/web.php
+++ routes/web.php
@@ -71,10 +71,55 @@ Route::get('/deconnexion', 'DeconnexionControler@deconnexion')->name('vue_deconn
 // Pour afficher tous les utilisateurs 
 
 Route::get('/admin_accueil', 'AdminAffController@accueil')->name('admin_accueil');
+
+
+
+
+
 //  Tous les eleves
-Route::get('/All_Students', 'AdminAffController@allstudent')->name('all_student');
+Route::get('/AllStudent', 'AdminAffController@allstudent')->name('admin_allstudent');
 //Detail sur les eleves 
-// Route::get('/admin',)
+Route::get('/detailstudent', 'AdminAffController@detailstudent')->name('admin_detailstudent');
+// Ajouter eleve
+Route::get('/AddStudent', 'AdminAffController@addstudent')->name('admin_addstudent');
+// tous les profs
+Route::get('/Allallteacher', 'AdminAffController@allteacher')->name('admin_allallteacher');
+// details profs
+Route::get('/Detailteacher', 'AdminAffController@detailteacher')->name('admin_detailteacher');
+// ajouter prof 
+Route::get('/Addteacher', 'AdminAffController@addteacher')->name('admin_addteacher');
+// tous les parents
+Route::get('/allparent', 'AdminAffController@allparent')->name('admin_allparent');
+// details parents
+Route::get('/detailparent', 'AdminAffController@detailparent')->name('admin_aldetailparent');
+// ajouter parent
+Route::get('/addparent', 'AdminAffController@addparent')->name('admin_addparent');
+// frais scolaire
+Route::get('/fraisscolaire', 'AdminAffController@fraisscolaire')->name('admin_fraisscolaire');
+// depenses 
+Route::get('/depense', 'AdminAffController@depense')->name('admin_depense');
+// ajouter frais 
+Route::get('/addfrais', 'AdminAffController@addfrais')->name('admin_addfrais');
+// toutes les classes
+Route::get('/allclass', 'AdminAffController@allclass')->name('admin_allclass');
+// ajouter classe 
+Route::get('/addclass', 'AdminAffController@addclass')->name('admin_addclass');
+// examen horaire 
+Route::get('/horaire', 'AdminAffController@horaire')->name('admin_horaire');
+// examen note 
+Route::get('/mark', 'AdminAffController@mark')->name('admin_mark');
+// matieres 
+Route::get('/subject', 'AdminAffController@subject')->name('admin_subject');
+// classe routine 
+Route::get('/classroutine', 'AdminAffController@classroutine')->name('admin_classroutine');
+// presence 
+Route::get('/presence', 'AdminAffController@presence')->name('admin_presence');
+// notification 
+Route::get('/notice', 'AdminAffController@notice')->name('admin_notice');
+// messages 
+Route::get('/message', 'AdminAffController@Message')->name('admin_message');
+//  compte 
+Route::get('/account', 'AdminAffController@account')->name('admin_account');
 
 
 
