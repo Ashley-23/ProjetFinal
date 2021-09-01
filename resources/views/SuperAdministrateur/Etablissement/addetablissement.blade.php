04406e1a97c761af3988b08385a6414e0c9279de
Ashley-23 <ashleysatchivi92@gmail.com>
2021-09-01 12:08:19 +0000

    Ajout des elements des interfaces du super administrateur

--- resources/views/SuperAdministrateur/Etablissement/addetablissement.blade.php
+++ resources/views/SuperAdministrateur/Etablissement/addetablissement.blade.php
@@ -267,4 +267,182 @@
 
 
 
+
+
+
+
+
+             <!-- Sidebar Area End Here -->
+             <div class="dashboard-content-one">
+
+
+
+
+
+
+                <!-- Breadcubs Area Start Here -->
+                <div class="breadcrumbs-area">
+                    <h3>Etablissement</h3>
+                    <ul>
+                        <li>
+                            <a href="index-2.html">Home</a>
+                        </li>
+                        <li>Ajouter etablissement</li>
+                    </ul>
+                </div>
+                <!-- Breadcubs Area End Here -->
+                <!-- Admit Form Area Start Here -->
+                <div class="card height-auto">
+                    <div class="card-body">
+                        <div class="heading-layout1">
+                            <div class="item-title">
+                                <h3>Ajouter Etablissement</h3>
+                            </div>
+                            <div class="dropdown">
+                                <a class="dropdown-toggle" href="#" role="button" data-toggle="dropdown"
+                                    aria-expanded="false">...</a>
+
+                                <div class="dropdown-menu dropdown-menu-right">
+                                    <a class="dropdown-item" href="#"><i
+                                            class="fas fa-times text-orange-red"></i>Close</a>
+                                    <a class="dropdown-item" href="#"><i
+                                            class="fas fa-cogs text-dark-pastel-green"></i>Edit</a>
+                                    <a class="dropdown-item" onClick="history.go(0)" href="#"><i
+                                            class="fas fa-redo-alt text-orange-peel" onClick="history.go(0)"></i>Refresh</a>
+                                </div>
+                            </div>
+                        </div>
+                        <form class="new-added-form">
+                            <div class="row">
+                                <div class="col-xl-3 col-lg-6 col-12 form-group">
+                                    <label>Nom </label>
+                                    <input type="text" placeholder="" class="form-control">
+                                </div>
+                                <div class="col-xl-3 col-lg-6 col-12 form-group">
+                                    <label>Prénom(s)</label>
+                                    <input type="text" placeholder="" class="form-control">
+                                </div>
+                                <div class="col-xl-3 col-lg-6 col-12 form-group">
+                                    <label>Genre</label>
+                                    <select class="select2">
+                                        {{-- <option value="">Please Select Gender *</option> --}}
+                                        <option value="m">Masculin</option>
+                                        <option value="f">Feminin</option>
+                                        <option value="3">Others</option>
+                                    </select>
+                                </div>
+                                <div class="col-xl-3 col-lg-6 col-12 form-group">
+                                    <label>Date de naissance</label>
+                                    <input type="date" placeholder="dd/mm/yyyy" class="form-control air-datepicker"
+                                        data-position='bottom right'>
+                                    <i class="far fa-calendar-alt"></i>
+                                </div>
+                                <div class="col-xl-3 col-lg-6 col-12 form-group">
+                                    <label>Login</label>
+                                    <input type="text" placeholder="" class="form-control">
+                                </div>
+                                <div class="col-xl-3 col-lg-6 col-12 form-group">
+                                    <label>Blood Group *</label>
+                                    <select class="select2">
+                                        <option value="">Please Select Group *</option>
+                                        <option value="1">A+</option>
+                                        <option value="2">A-</option>
+                                        <option value="3">B+</option>
+                                        <option value="3">B-</option>
+                                        <option value="3">O+</option>
+                                        <option value="3">O-</option>
+                                    </select>
+                                </div>
+                                <div class="col-xl-3 col-lg-6 col-12 form-group">
+                                    <label>Religion *</label>
+                                    <select class="select2">
+                                        <option value="">Please Select Religion *</option>
+                                        <option value="1">Islam</option>
+                                        <option value="2">Hindu</option>
+                                        <option value="3">Christian</option>
+                                        <option value="3">Buddish</option>
+                                        <option value="3">Others</option>
+                                    </select>
+                                </div>
+                                <div class="col-xl-3 col-lg-6 col-12 form-group">
+                                    <label>E-Mail</label>
+                                    <input type="email" placeholder="" class="form-control">
+                                </div>
+                                <div class="col-xl-3 col-lg-6 col-12 form-group">
+                                    <label>Classe</label>
+                                    <div class="classes" id="choixclasses">
+                                        <select class="select2" onchange="chargertype()">
+                                            <option value="s">selectionnez une classe</option>
+                                            <option value="sixieme">sixieme</option>
+                                            <option value="cinquieme">cinquieme</option>
+                                            <option value="quatrieme">quatrieme</option>
+                                            <option value="troisieme">troisieme</option>
+                                            <option value="seconde">seconde</option>
+                                            <option value="premiere">premiere</option>
+                                            <option value="terminale">terminale</option>
+                                        </select>
+
+                                        
+                                            <div class="typeclasse" id="typeclasse">       
+                                                {{-- Pour l'affichage des options selon la classe selectionné  --}}
+
+
+                                                &nbsp;
+                                                {{-- <label>Section *</label>
+                                                <select class="select2">
+                                                    <option value="">Please Select Section *</option>
+                                                    <option value="1">Pink</option>
+                                                    <option value="2">Blue</option>
+                                                    <option value="3">Bird</option>
+                                                    <option value="3">Rose</option>
+                                                    <option value="3">Red</option>
+                                                </select> --}}
+                                            </div>
+                                    </div>
+                                    
+                                </div>
+                                
+                                <div class="col-xl-3 col-lg-6 col-12 form-group">
+                                    <label>Adresse</label>
+                                    <input type="text" placeholder="" class="form-control">
+                                </div>
+                                <div class="col-xl-3 col-lg-6 col-12 form-group">
+                                    <label>Telephone</label>
+                                    <input type="text" placeholder="" class="form-control">
+                                </div>
+                                {{-- <div class="col-lg-6 col-12 form-group">
+                                    <label>Short BIO</label>
+                                    <textarea class="textarea form-control" name="message" id="form-message" cols="10"
+                                        rows="9"></textarea>
+                                </div> --}}
+                                <div class="col-lg-6 col-12 form-group mg-t-30">
+                                    <label class="text-dark-medium">Upload Student Photo (150px X 150px)</label>
+                                    <input type="file" class="form-control-file">
+                                </div>
+                                <div class="col-12 form-group mg-t-8">
+                                    <button type="submit" class="btn-fill-lg btn-gradient-yellow btn-hover-bluedark">Save</button>
+                                    <button type="reset" class="btn-fill-lg bg-blue-dark btn-hover-yellow">Reset</button>
+                                </div>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+                <!-- Admit Form Area End Here -->
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 @endsection
--- resources/views/SuperAdministrateur/Etablissement/alletablissement.blade.php
+++ resources/views/SuperAdministrateur/Etablissement/alletablissement.blade.php
@@ -262,6 +262,136 @@
             </div>
             <!-- Sidebar Area End Here -->
 
+<!-- Sidebar Area End Here -->
+<div class="dashboard-content-one">
+    <!-- Breadcubs Area Start Here -->
+    <div class="breadcrumbs-area">
+        <h3>Etablissement</h3>
+        <ul>
+            <li>
+                <a href="index-2.html">Home</a>
+            </li>
+            <li>Tous les etablissements</li>
+        </ul>
+    </div>
+    <!-- Breadcubs Area End Here -->
+    <!-- Student Table Area Start Here -->
+    <div class="card height-auto">
+        <div class="card-body">
+            <div class="heading-layout1">
+                <div class="item-title">
+                    <h3>Tous les etablissements</h3>
+                </div>
+                <div class="dropdown">
+                    <a class="dropdown-toggle" href="#" role="button" data-toggle="dropdown"
+                        aria-expanded="false">...</a>
+
+                    <div class="dropdown-menu dropdown-menu-right">
+                        <a class="dropdown-item" href="#"><i
+                                class="fas fa-times text-orange-red"></i>Close</a>
+                        <a class="dropdown-item" href="#"><i
+                                class="fas fa-cogs text-dark-pastel-green"></i>Edit</a>
+                        <a class="dropdown-item" href="#"><i
+                                class="fas fa-redo-alt text-orange-peel" onClick="history.go(0)" ></i>Refresh</a>
+                    </div>
+                </div>
+            </div>
+            <form class="mg-b-20">
+                <div class="row gutters-8">
+                    <div class="col-3-xxxl col-xl-3 col-lg-3 col-12 form-group">
+                        <input type="text" placeholder="Search by Roll ..." class="form-control">
+                    </div>
+                    <div class="col-4-xxxl col-xl-4 col-lg-3 col-12 form-group">
+                        <input type="text" placeholder="Search by Name ..." class="form-control">
+                    </div>
+                    <div class="col-4-xxxl col-xl-3 col-lg-3 col-12 form-group">
+                        <input type="text" placeholder="Search by Class ..." class="form-control">
+                    </div>
+                    <div class="col-1-xxxl col-xl-2 col-lg-3 col-12 form-group">
+                        <button type="submit" class="fw-btn-fill btn-gradient-yellow">SEARCH</button>
+                    </div>
+                </div>
+            </form>
+            <div class="table-responsive">
+                <table class="table display data-table text-nowrap">
+                    <thead>
+                        <tr>
+                            <th>
+                                <div class="form-check">
+                                    <input type="checkbox" class="form-check-input checkAll">
+                                    <label class="form-check-label">Roll</label>
+                                </div>
+                            </th>
+                            <th>Id</th>
+                            <th>Nom</th>
+                            <th>Prenom</th>
+                            <th>Login</th>
+                            <th>Date de naissance</th>
+                            <th>Genre</th>
+                            <th>Email</th>
+                            <th>Telephone</th>
+                            <th>Photo</th>
+                            <th>Adresse</th>
+                            <th>Active</th>
+                            <th></th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {{-- <tr>
+
+                            @forelse ($adminAff as $eleve)
+                                
+                                  <td>
+                                                    <div class="form-check">
+                                                        <input type="checkbox" class="form-check-input">
+                                                        <label class="form-check-label">#0021</label>
+                                                    </div>
+                                                </td>
+                                                <td class="text-center"><img src="img/figure/student2.png" alt="student"></td>
+                                                <td>  <a href=""> {{eleve->nomEleve}} </a> </td>
+                                                <td>  <a href=""> {{eleve->prenomEleve}} </a> </td>
+                                                <td>  <a href=""> {{eleve->login}} </a> </td>
+                                                <td>  <a href=""> {{eleve->dateNaissEleve}} </a> </td>
+                                                <td>  <a href=""> {{eleve->sexeEleve}} </a>  </td>
+                                                <td>  <a href=""> {{eleve->emailEleve}} </a> </td>
+                                                <td>  <a href=""> {{eleve->telephoneEleve}} </a> </td>
+                                                <td>  <a href=""> {{eleve->photoEleve}} </a> </td>
+                                                <td>  <a href=""> {{eleve->adresseEleve}} </a> </td>
+                                                <td>  <a href=""> {{eleve->activeEleve}} </a> </td>
+                                                <td>
+                                                    <div class="dropdown">
+                                                        <a href="#" class="dropdown-toggle" data-toggle="dropdown"
+                                                            aria-expanded="false">
+                                                            <span class="flaticon-more-button-of-three-dots"></span>
+                                                        </a>
+                                                        <div class="dropdown-menu dropdown-menu-right">
+                                                            <a class="dropdown-item" href="#"><i
+                                                                    class="fas fa-times text-orange-red"></i>Close</a>
+                                                            <a class="dropdown-item" href="#"><i
+                                                                    class="fas fa-cogs text-dark-pastel-green"></i>Edit</a>
+                                                            <a class="dropdown-item" href="#"><i
+                                                                    class="fas fa-redo-alt text-orange-peel"></i>Refresh</a>
+                                                        </div>
+                                                    </div>
+                                                </td>
+
+
+                            @empty
+                                <p>Aucun élève dans la base de donnée </p>
+                            @endforelse
+                        
+                            
+                        </tr>       --}}
+
+
+<h2> Affichage</h2>
+
+
+
+                    </tbody>
+                </table>
+            </div>
+        </div>
 
 
 
