78829caf27a7abe65ad3eeb8bf21d0d02ad25684
Ashley-23 <ashleysatchivi92@gmail.com>
2021-09-02 09:40:38 +0000

    Ajout d'un select dans views/Auth/register.blade.php pour le choix des roles et ajout de la commande ->attachRole(->role_id); dans App/Http/Controller/Auth/RegisterUserController.php pour qu'il choisisse automatiquemeent le rôle selon l'id

--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -48,7 +48,7 @@ class RegisteredUserController extends Controller
         event(new Registered($user));
 
         Auth::login($user);
-        $user->attachRole('admin');
+        $user->attachRole($request->role_id);
 
         return redirect(RouteServiceProvider::HOME);
     }
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -14,7 +14,7 @@
 
             <!-- Name -->
             <div>
-                <x-label for="name" :value="__('Name')" />
+                <x-label for="name" :value="__('Nom')" />
 
                 <x-input id="name" class="block mt-1 w-full" type="text" name="name" :value="old('name')" required autofocus />
             </div>
@@ -28,7 +28,7 @@
 
             <!-- Password -->
             <div class="mt-4">
-                <x-label for="password" :value="__('Password')" />
+                <x-label for="password" :value="__('Mot de passe ')" />
 
                 <x-input id="password" class="block mt-1 w-full"
                                 type="password"
@@ -38,12 +38,34 @@
 
             <!-- Confirm Password -->
             <div class="mt-4">
-                <x-label for="password_confirmation" :value="__('Confirm Password')" />
+                <x-label for="password_confirmation" :value="__('Confirmer le mot de passe ')" />
 
                 <x-input id="password_confirmation" class="block mt-1 w-full"
                                 type="password"
                                 name="password_confirmation" required />
             </div>
+            {{-- Ajout de l'option select pour les rôles  --}}
+            <div class="mt-4">
+                <x-label for="role_id" :value="__(' S\'enregistrer en tant que ')" />
+        
+               <select name="role_id" class="block mt-1 w-full border-gray-300
+               focus:border-indigo-300 focus:ring-indigo-200
+               focus:ring-opacity-50 rounded-md shaddow-sm ">
+
+                    <option value="eleve"> Eleve </option>
+                    <option value="Relative"> Parent </option>
+                    <option value="professeur"> Professeur </option>
+                    <option value="etablissement"> Etablissement </option>
+                    <option value="Admin"> Administrateur </option>
+                    <option value="SuperAdmin"> Super Administrateur </option>
+
+                </select>
+
+            </div>
+
+          
+
+            {{-- Fin de l'ajout des options select pour les rôles  --}}
 
             <div class="flex items-center justify-end mt-4">
                 <a class="underline text-sm text-gray-600 hover:text-gray-900" href="{{ route('login') }}">
